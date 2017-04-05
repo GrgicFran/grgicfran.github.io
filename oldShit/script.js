e596150b70901f4843630488c137eaaf89ecb72d
GrgaMrga <grgicfran@gmail.com>
2017-04-05 16:16:06 +0200

    removed unuseful stuff

--- oldShit/script.js
+++
@@ -1,9 +0,0 @@
-var a = document.querySelectorAll(".skill");
-var toggle = false;
-
-function handleUpdate() {
-    if (this.style.height == "100px") this.style.height = "35px";
-    else this.style.height = "100px";
-}
-
-a.forEach(element => element.addEventListener('click', handleUpdate));
